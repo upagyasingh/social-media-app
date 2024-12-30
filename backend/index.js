3b88f058a233253aa2aec6ce1a947b30c74b8e62
upagyasingh <upagyasinght48@gmail.com>
2024-12-30 10:47:35 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -28,12 +28,11 @@ app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/notifications",notificationRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if(process.env.NODE_ENV==="production"){ app.use(express.static(path.join(__dirname,"frontend/dist")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-  });
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+  })
 }
 
 app.get("/",(req,res)=>{
